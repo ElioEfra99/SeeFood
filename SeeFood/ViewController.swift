b5fb57a5d4343f3aae6821728a52653bd9d273d2
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2020-08-14 15:23:20 -0500

    Initial commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Eliu Efraín Díaz Bravo on 14/08/20.
+//  Copyright © 2020 Eliu Díaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
